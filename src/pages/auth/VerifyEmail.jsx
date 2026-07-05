4cf83833177a02a1331cb0f67559003444e76cec
ayusha1223 <ayushathapa425@gmail.com>
2026-07-05 14:39:29 +0545

    feat(frontend): implement email verification page

--- src/pages/auth/VerifyEmail.jsx
+++ src/pages/auth/VerifyEmail.jsx
@@ -1,5 +1,46 @@
+import { useEffect } from "react";
+import { useNavigate, useParams } from "react-router-dom";
+import toast from "react-hot-toast";
+
+import api from "../../api/axios";
+
 const VerifyEmail = () => {
-  return <h1>Verify Email Page</h1>;
+  const { token } = useParams();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const verify = async () => {
+      try {
+        const { data } = await api.get(
+          `/auth/verify-email/${token}`
+        );
+
+        toast.success(data.message);
+
+        navigate("/login");
+      } catch (err) {
+        toast.error(
+          err.response?.data?.message ||
+            "Verification failed"
+        );
+      }
+    };
+
+    verify();
+  }, [token, navigate]);
+
+  return (
+    <div
+      style={{
+        height: "100vh",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <h2>Verifying your email...</h2>
+    </div>
+  );
 };
 
 export default VerifyEmail;
